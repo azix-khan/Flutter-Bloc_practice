eb4ed0d7019fbd6bae98e20cd944b8089ce9120d
azix khan <azixkhan.55@gmail.com>
2024-05-20 23:46:49 +0500

    switch and slider

---
+++ android/app/src/main/kotlin/com/example/switch_slider_witt_bloc/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.switch_slider_witt_bloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/UI/Switch_Slider/switch_slider_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class SwitchSliderScreen extends StatefulWidget {
+  const SwitchSliderScreen({super.key});
+
+  @override
+  State<SwitchSliderScreen> createState() => _SwitchSliderScreenState();
+}
+
+class _SwitchSliderScreenState extends State<SwitchSliderScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Practice with Bloc on switch and slider"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text("Notification"),
+                Switch(value: true, onChanged: (newValue) {}),
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              height: 200,
+              color: Colors.red.withOpacity(.2),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Slider(value: .4, onChanged: (value) {}),
+          ],
+        ),
+      ),
+    );
+  }
+}
